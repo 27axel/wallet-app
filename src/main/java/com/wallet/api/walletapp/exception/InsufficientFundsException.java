a6577bc7edf0070396f09551f918037831647b2a
axel <i@amusaev.ru>
2024-05-24 18:46:59 +0300

    fix response status InsufficientFundsException

--- src/main/java/com/wallet/api/walletapp/exception/InsufficientFundsException.java
+++ src/main/java/com/wallet/api/walletapp/exception/InsufficientFundsException.java
@@ -3,7 +3,7 @@ package com.wallet.api.walletapp.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class InsufficientFundsException extends RuntimeException {
     public InsufficientFundsException(String message) {
         super(message);
